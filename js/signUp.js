57f342868d4e7c172682718f70b9f7ed58d9a1d0
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2024-02-14 18:18:54 -0500

    change signUp.js to signup.js

--- js/signUp.js
+++ js/signUp.js
@@ -2,28 +2,6 @@ Parse.initialize("erMai5U9yDcuMfH47yV3DBkzK5DJu9nLtfuM4VLm", "F5sG1HntHNFByrRpHm
 Parse.serverURL = "https://parseapi.back4app.com/";
 
 
-async function login() {
-	const username = document.getElementById('username').value;
-	const password = document.getElementById('password').value;
-
-	try {
-		let user = await Parse.User.logIn(username, password);
-		// login success
-		console.log("User successfully logged in", user);
-		location.href = 'index.html';
-	}
-	catch (error) {
-		// login failed
-		console.error("Error while logging in user", error);
-	}
-} 
-
-document.getElementById("login").addEventListener("click", async function () {
-  login();
-});
-
-
-
 async function signUp() {
 	let user = new Parse.User();
 	user.set("username", document.getElementById("username").value);
@@ -35,12 +13,13 @@ async function signUp() {
 			alert(
 				`New username created! Hello ${user.get("username")}`
 			);
+			location.href = 'index.html';
 		}
 	} catch (error) {
 		alert(`Error: ${error.message}`);
 	}
 }
 
-document.getElementById("createButton").addEventListener("click", async function () {
+document.getElementById("signUp").addEventListener("click", async function () {
 	signUp();
 });
