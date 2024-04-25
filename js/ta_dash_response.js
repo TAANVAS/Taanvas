2b9cca29790ac59451a88beefed95d7eff5ccfef
Caleb Panoch <123989314+calebpanoch@users.noreply.github.com>
2024-04-25 15:51:46 -0400

    Update TA Committee Dash
    add login verification
    
    add applications and student info

--- js/ta_dash_response.js
+++ js/ta_dash_response.js
@@ -3,6 +3,88 @@ Parse.serverURL = "https://parseapi.back4app.com/";
 
 const currentUser = Parse.User.current();
 
+if (currentUser) {
+    if (!currentUser.get("IsCommittee")) {
+        alert("You are not logged in as a Committee Member.")
+        location.href = '../index.html'
+    }
+} else {
+    alert("You are not logged in.")
+    location.href = '../login.html';
+    console.log("NOT LOGGED IN!")
+}
+
+var Users = Parse.Object.extend("User");
+var Reports = Parse.Object.extend("Report");
+var Courses = Parse.Object.extend("Course");
+var Applications = Parse.Object.extend("Application");
+
+
+var loading = false
+
+var event = new Event('input');
+
+
+// Code to execute whenever input is changed
+var table = document.getElementById('pendingTable')
+
+
+//table.innerHTML = ''
+
+
+appQuery = new Parse.Query(Applications);
+let statusValues = [-2,3];
+
+// Create a query for objects where the "Status" field is in the statusValues array
+appQuery.containedIn("Status", statusValues)
+appQuery.include("User");
+const tableBody = document.querySelector('#applicationTable tbody');
+appQuery.find().then(function(results) {
+    // Loop through each object in the results array
+    results.forEach(function(result) {
+        //define constants for each object that you want to get
+        var user = result.get("User")
+        const studentName = user.get('username');
+        const studentZNumber = user.get("Znumber")
+        
+        const courseName = result.get('AcceptedCourse');
+        var accepted = result.get("Status")
+        if (accepted == -2) {
+            accepted = "Accepted by Student"
+        } else {
+            accepted = "Declined by Student"
+        }
+
+        const row = document.createElement('tr');
+
+
+
+        row.innerHTML = `
+                <td class="py-2 px-4 border-b border-gray-300 text-center">${studentName}</td>
+                <td class="py-2 px-4 border-b border-gray-300 text-center">${studentZNumber}</td>
+                <td class="py-2 px-4 border-b border-gray-300 text-center">${courseName}</td>
+                <td class="py-2 px-4 border-b border-gray-300 text-center">${accepted}</td>
+            `;
+        tableBody.appendChild(row);
+    });
+});
+
+/*Parse.initialize("erMai5U9yDcuMfH47yV3DBkzK5DJu9nLtfuM4VLm", "F5sG1HntHNFByrRpHmzJXylHE1e9PJHBAiqOhNCx");
+Parse.serverURL = "https://parseapi.back4app.com/";
+
+const currentUser = Parse.User.current();
+
+if (currentUser) {
+    if (!currentUser.get("IsApplicant")) {
+        alert("You are not logged in as a TA Applicant.")
+        location.href = '../index.html'
+    }
+} else {
+    alert("You are not logged in.")
+    location.href = '../login.html';
+    console.log("NOT LOGGED IN!")
+}
+
 if (currentUser) {    
     const Course = Parse.Object.extend("Course");
     const query = new Parse.Query(Course);
@@ -48,4 +130,4 @@ if (currentUser) {
 }
 
 
-console.log("Script has been loaded");
+console.log("Script has been loaded");*/
